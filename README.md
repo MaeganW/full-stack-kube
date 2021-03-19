41ca1f0a1f3b1b3225e606d47cc3525028643148
Maegan Womble <maegan.womble@e-hps.com>
2021-03-19 14:52:20 -0500

    fixed cors issue

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # full-stack-kube
+Full-stack Kubernetes project practice - Express NodeJs and React
 ## Docker Images
 * maeganwomble/react-pong
 * maeganwomble/node-pong
--- node-pong/index.js
+++ node-pong/index.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
+
 app.listen(3001, function () {
-  console.log("listening on 3001");
+    console.log("listening on 3001");
 });
 
 app.get("/", (req, res) => {
-  res.send("Api is running");
+    res.send("Api is running");
 });
 
 app.get("/ping", (req, res) => {
-  res.send("pong");
+    setTimeout(() => {
+        res.send("pong");
+    }, 1000);
 });
